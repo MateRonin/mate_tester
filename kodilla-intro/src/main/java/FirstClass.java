a057fcbb9927b4183fdf8f58a3850bcf9fb99a35
Mate <sendecki.mateusz@gmail.com>
2021-06-15 22:03:44 +0200

    Hello World

---
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -0,0 +1,9 @@
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello from FirstClass!");
+        System.out.println("Mateusz");
+        System.out.println(33);
+        System.out.println('S');
+    }
+
+}
---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Mateusz");
+    }
+
+    public static void main(int[] args) {
+        System.out.println(33);
+    }
+
+    public static void main(char[] args) {
+        System.out.println('S');
+    }
+}
