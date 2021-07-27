afadce56882b1535bfffcdb985154f6c8e18c08a
Mate <sendecki.mateusz@gmail.com>
2021-07-27 19:38:15 +0200

    Jobs

---
+++ kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homework/Doctor.java
@@ -0,0 +1,9 @@
+package com.kodilla.abstracts.homework;
+
+public class Doctor extends Job {
+    public Doctor() {
+        super(20000, "operating, taking care of the patients, managing hospital personnel");
+    }
+
+
+}
---
+++ kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homework/Lawyer.java
@@ -0,0 +1,8 @@
+package com.kodilla.abstracts.homework;
+
+public class Lawyer extends Job {
+    public Lawyer() {
+        super(10000,"representing clients in courts");
+    }
+
+}
