c71d46d0bd12d5d592ef5bf9291b7acffd2d7197
Mate <sendecki.mateusz@gmail.com>
2021-09-13 22:59:59 +0200

    Cars

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_configuration/homework/CarChoice.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_configuration/homework/CarChoice.java
@@ -3,42 +3,29 @@ package com.kodilla.spring.basic.spring_configuration.homework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.time.LocalTime;
-import java.util.Random;
+import java.time.LocalDate;
 
 @Configuration
 public class CarChoice {
 
     private Car car;
-    private Seasons seasons;
 
     @Bean
     public Car chooseCar() {
-        {
-            Random random = new Random();
-            int number = random.nextInt(4 + 1);
-            switch (number) {
-                case 1:
-                    seasons = Seasons.SUMMER;
-                    car = new Cabrio();
-                    break;
-                case 2:
-                    seasons = Seasons.SPRING;
-                    car = new Sedan();
-                    break;
-                case 3:
-                    seasons = Seasons.WINTER;
-                    car = new SUV();
-                    break;
-                case 4:
-                    seasons = Seasons.AUTUMN;
-                    car = new Sedan();
-                    break;
-            }
-            return car;
+        if (LocalDate.now().getMonthValue() >=1 || LocalDate.now().getMonthValue() <=3) {
+            car = new SUV();
+
+        } else if (LocalDate.now().getMonthValue() >=7 || LocalDate.now().getMonthValue() <=9) {
+            car = new Cabrio();
+
+        } else {
+            car = new Sedan();
         }
+        return car;
     }
 }
+
+
 
 
 
